b40f5f9e65e69a7baff24193456c3ee56f86dbce
13eMualliug <47554403+13eMualliug@users.noreply.github.com>
2019-03-07 12:29:33 +0100

    Update run.py

--- run.py
+++ run.py
@@ -13,12 +13,14 @@
 # * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # * See the License for the specific language governing permissions and
 # * limitations under the License.
-
-def main(argv):
+   
+        
+def main():
     while True:
-      # code
-      if condition:
-        break
+        # code
+        if condition:
+            break
 
 if __name__ == "__main__":
-    import sys
+    main()
+    
